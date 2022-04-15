154ce8a5e270a6d18dc2ff5399c1b0c4b62c587c
jonatan <jaumaf@hotmail.com>
2022-04-15 18:03:34 -0500

    - formula muestra para 4 experimentos más, dando un total de 5 corridas - aun no lo corro

---
+++ jakobson/corpus_objetivo.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 15 15:50:26 2022
+
+@author: jaumaf
+"""
+
+ 
+c1= {
+        "a40": "People. Art & Education",
+        "b27": "Letters to the Editor",
+        "c17": "Reviews",
+        "d09": "Organizing the Local Church",
+        "e36": "Renting a Car in Europe",
+        "f48": "Christian Ethics & the Sit-In",
+        "g75": "A Wreath for Garibaldi",
+        "h30": "Annual Report of Year Ending June 30, 1961",
+        "j80": "Principles of Inertial Navigation",
+        "k29": "The Sheep's in the Meadow",
+        "l24": "The Murders",
+        "m02": "The Lovers",
+        "n29":  "Riding the Dark Train Out",
+        "p20": "Dirty Dig Inn"
+        }
+    
+c2 = {
+        "a02": " The Dallas Morning News",
+        "b01": " The Atlanta Constitution,",
+        "c01": "Chicago Daily Tribune",
+        "d01": "William G. Pollard, Physicist and Christian",
+        "e02": "Organic Gardening and Farming,",
+        "f01": "How Much Do You Tell When You Talk?",
+        "g01": "Northern Liberals and Southern Bourbons",
+        "h01": "Handbook of Federal Aids to Communities. U.S. Department of",
+        "j01": "Radio Emission of the Moon and Planet",
+        "k01": "Christopher Davis, First Family.",
+        "l02": "Fair, Bachelors Get Lonely",
+        "m01": "Stranger in a Strange Land.",
+        "n02":  "The Valley",
+        "p01": " A Cup of the Sun"
+        }
+  
+c3 =     {
+"A03" : "Chicago Daily Tribune",
+"B02" : "The Christian Science Monitor",
+"C02" : "The Christian Science Monitor",
+"D03" : "Christian Unity in England",
+"E03" : "Will Aircraft or Missiles Win Wars?",
+"F02" : "America's Secret Poison Gas Tragedy",
+"G02" : "Toward a Concept of National Responsibility",
+"H02" : "An Act for International Development, A Summary Presentation",
+"J02" : "Proceedings of the 1961 Heat",
+"K02" : "The Ikon",
+"L03" : "Encounter with Evil",
+"M03" : "The Star Dwellers",
+"N03" : "Trail of the Tattered Star",
+"P02" : "Seize a Nettle"
+}
+  
+  
+c4 = {
+"A04": "The Christian Science Monitor",
+"B04": "The Miami Herald, September",
+"C03": "The New York Times",
+"D05": "Theodore Parker: Apostasy within Liberalism",
+"E04": "High Fidelity",
+"F03": "I've Been Here before!",
+"G03": "The Chances of Accidental War",
+"H03": "87th Congress, 1st Session. House Document No. 247. The",
+"J03": "The Normal Forces and Their Thermodynamic Significance",
+"K03": "Tristram Coffin, Not to the Swift",
+"L06": "Hunter at Large",
+"M04": "The Planet with No Nightmare",
+"N04": "The Shadow Catcher",
+"P03": "The Fairbrothers"
+}
+
+
+c5 = {
+"A05": "The Providence Journal",
+"B05": "Newark Evening News",
+"C04": "The Providence Journal",
+"D06": "Tracts published by American Tract Society",
+"E07": "How to design your Interlocking Frame",
+"F04": "North Country School Cares for the Whole Child",
+"G04": "The Invisible Aborigine",
+"H04": "Rhode Island Legislative Council. Research Report Number 1",
+"J04": "Proton magnetic resonance study",
+"K05": "The Judges of the Secret Court",
+"L07": "Deadlier Than the Male.",
+"M05": "The Ship Who Sang",
+"N06": "Here Comes Pete Now.",
+"P04": "The Moon and the Thorn."
+}
